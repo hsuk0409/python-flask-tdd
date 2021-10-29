2f93d319fa7feac8170b676e4de062e6331b75d2
ChoinSuk <hsuk0409@gmail.com>
2021-10-29 22:22:29 +0900

    user 생성 api 테스트 케이스 추가, json loads 컨버터 사용

--- web/test_app.py
+++ web/test_app.py
@@ -21,11 +21,8 @@ def test_index(api):
 
 
 def test_user(api):
-    new_user = {
-        "email": "hsuk@thecommerce.co.kr",
-        "name": "justin",
-        "role": "A"
-    }
+    new_user = set_user("hsuk@thecommerce.co.kr",
+                        "justin")
 
     response = api.post(
         "/sign-up",
@@ -37,3 +34,26 @@ def test_user(api):
     print(f"response status code: {response.status_code}")
     print(f"response data: {response.data}")
     assert response.status_code == 200
+
+    new_user = set_user("hsuk22@thecommerce.co.kr",
+                        "justin22")
+    response = api.post(
+        "/sign-up",
+        data=json.dumps(new_user),
+        content_type="application/json"
+    )
+
+    print(f"response data: {response.data}")
+    assert response.status_code == 200
+
+    json_dic = json.loads(response.data)
+    print(f"converted json: {json_dic}")
+    assert json_dic.get("id") == 2
+
+
+def set_user(email: str, name: str) -> dict:
+    return {
+        "email": email,
+        "name": name,
+        "role": "A"
+    }
