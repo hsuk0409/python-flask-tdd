fa1ea8fb5ba0994591cb3bba8e33820b38d85626
chionsuk <hsuk0409@gmail.com>
2021-11-30 21:45:27 +0900

    파이썬 datetime 테스트

--- tests/test_something.py
+++ tests/test_something.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 
 def convert_datetime_to_date_str(dt: datetime) -> str:
@@ -13,3 +13,21 @@ def convert_datetime_to_date_str(dt: datetime) -> str:
 def test_convert_datetime():
     result = str(datetime.now().date())
     assert convert_datetime_to_date_str(datetime.now()) == result
+
+
+def test_date_today():
+    today = date.today()
+    tomorrow = today + timedelta(1)
+    print()
+    print(f"today:: {today}")
+    print(f"tomorrow:: {tomorrow}")
+
+    assert tomorrow > today
+
+
+def test_str_date_convert():
+    str_date = "2021-11-30"
+    converted_date = datetime.strptime(str_date, "%Y-%m-%d").date()
+
+    print(f"converted_date:: {converted_date}")
+
