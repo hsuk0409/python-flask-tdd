835aa61cbb90055deabbf73183a90a99e09bba2f
chionsuk <hsuk0409@gmail.com>
2022-03-26 22:40:38 +0900

    파이선 변수 값 교환 #2

--- tests/test_variable_swap.py
+++ tests/test_variable_swap.py
@@ -1,8 +1,17 @@
-
 def test_swap():
     print("\n")
     a = 10
     b = 5
-    print(f"a={a}, b={b}")
+    print_vars(a, b)
     a, b = b, a
+    print_vars(a, b)
+
+    a = "Hi!"
+    b = "Justin!!"
+    print_vars(a, b)
+    a, b = b, a
+    print_vars(a, b)
+
+
+def print_vars(a, b):
     print(f"a={a}, b={b}")
